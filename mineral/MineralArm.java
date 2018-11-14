d3e9fcb888c4a0aa38c31b0639ca2d7769f25412
glingy <gregory.ling@students.tka.org>
2018-11-13 18:51:30 -0800

    Fixed a few errors.

--- mineral/MineralArm.java
+++ mineral/MineralArm.java
@@ -8,10 +8,5 @@ import org.firstinspires.ftc.teamcode.common.AssemblyManager;
 public abstract class MineralArm extends OpMode{
     public abstract void extend() throws InterruptedException;
 
-    public abstract void retract() throws InterruptedException;
-
     public abstract void raiseTo(int targetPosition) throws InterruptedException;
-
-
-    public abstract void stopRaise();
 }
